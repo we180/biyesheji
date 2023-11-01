a60f587adf85f7a52090e171bfd1800bc3b18fed
we180 <1805825699@qq.com>
2023-11-01 16:14:44 +0800

    限选模块（社会服务）可以写了
    从uniViewModel_xianxuan文件里复制就行 注意文件路径 包含视频 图片等很多东西路径 重复的东西改一下 稍后把素材发到群里

---
+++ pages/mypages/xianxuan/xianxuan_mulu.vue
@@ -0,0 +1,121 @@
+<template>
+	<view>
+		<view class="xianxuan_list">
+			<view class="xianxuan" @click="choose_mokuai(0)">
+				<view class="img">
+					<u-image src="../../../static/myimgs/xianxuan_mokuai/jingzhunfuping.png" width="100%" height="180rpx"></u-image>
+				</view>
+				<view class="text">
+					<text class="title">精准扶贫</text>
+					<text class="enter">点击进入</text>
+				</view>
+			</view>
+			<view class="xianxuan" @click="choose_mokuai(1)">
+				<view class="img">
+					<u-image src="../../../static/myimgs/xianxuan_mokuai/shidaikaimo.jpeg" width="100%" height="180rpx"></u-image>
+				</view>
+				<view class="text">
+					<text class="title">时代楷模</text>
+					<text class="enter">点击进入</text>
+				</view>
+			</view>
+			<view class="xianxuan" @click="choose_mokuai(2)">
+				<view class="img">
+					<u-image src="../../../static/myimgs/xianxuan_mokuai/zhihuidangjian.jpeg" width="100%" height="180rpx"></u-image>
+				</view>
+				<view class="text">
+					<text class="title">智慧党建</text>
+					<text class="enter">点击进入</text>
+				</view>
+			</view>
+			<view class="xianxuan"@click="choose_mokuai(3)">
+				<view class="img">
+					<u-image src="../../../static/myimgs/xianxuan_mokuai/zhihuihuanbao.jpeg" width="100%" height="180rpx"></u-image>
+				</view>
+				<view class="text">
+					<text class="title">智慧环保</text>
+					<text class="enter">点击进入</text>
+				</view>
+			</view>
+			<view class="xianxuan" @click="choose_mokuai(4)">
+				<view class="img">
+					<u-image src="../../../static/myimgs/xianxuan_mokuai/zhihuishequ.png" width="100%" height="180rpx"></u-image>
+				</view>
+				<view class="text">
+					<text class="title">智慧社区</text>
+					<text class="enter">点击进入</text>
+				</view>
+			</view>
+			<view class="xianxuan" @click="choose_mokuai(5)">
+				<view class="img">
+					<u-image src="../../../static/myimgs/xianxuan_mokuai/zhihuiyanglao.jpg" width="100%" height="180rpx"></u-image>
+				</view>
+				<view class="text">
+					<text class="title">智慧养老</text>
+					<text class="enter">点击进入</text>
+				</view>
+			</view>
+			<view class="xianxuan" @click="choose_mokuai(6)">
+				<view class="img">
+					<u-image src="../../../static/myimgs/xianxuan_mokuai/zhongguozhizao.png" width="100%" height="180rpx"></u-image>
+				</view>
+				<view class="text">
+					<text class="title">中国智造</text>
+					<text class="enter">点击进入</text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			choose_mokuai(id){
+				if(id==0){
+					console.log(id)
+				}else if(id==1){
+					console.log(id)
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.xianxuan_list{
+		display: flex;
+		flex-direction: column;
+	}
+	.xianxuan{
+		display: flex;
+		flex-direction: row;
+		margin: 6rpx;
+		padding: 8rpx;
+		border: solid 4rpx #999;
+		border-radius: 20rpx;
+		background-color: #F0F8FF;
+	}
+	.img{
+		flex: 1;
+	}
+	.text{
+		flex: 3;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+	.enter{
+		margin-top: 20rpx;
+		color: #888888;
+	}
+	.title{
+		font-size: 40rpx;
+	}
+</style>
